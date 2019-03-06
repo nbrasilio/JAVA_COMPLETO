a6d28c99c052989171bbeabfe0786545b1b5b937
Nelson Brasilio <nbrasilio@hotmail.com>
2019-03-06 14:15:28 -0300

    EXERCICIO FIXAÇÃO DE COMPOSIÇÃO

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,11 +1,16 @@
 package entities;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import entities.enums.OrderStatus;
 
 public class Order {
 
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
 	private Integer id;
 	private Date moment;
 	private OrderStatus status;
@@ -19,6 +24,15 @@ public class Order {
 		this.status = status;
 	}
 
+	private Client client;
+	private List<OrderItem> items = new ArrayList<>();
+	
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -43,8 +57,49 @@ public class Order {
 		this.status = status;
 	}
 
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+	
+	public void addItem(OrderItem item) {
+		items.add(item);
+	}
+
+	public void removeItem(OrderItem item) {
+		items.remove(item);
+	}
+	
+	public double total() {
+		double sum = 0.0;
+		for (OrderItem item : items) {
+			sum += item.subTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public String toString() {
-		return "Order [id=" + id + ", moment=" + moment + ", status=" + status + "]";
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order status: ");
+		sb.append(status + "\n");
+		sb.append("Client: ");
+		sb.append(client + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem item : items) {
+			sb.append(item + "\n");
+		}
+		sb.append("Total price: $");
+		sb.append(String.format("%.2f", total()));
+		return sb.toString();
 	}
 }
